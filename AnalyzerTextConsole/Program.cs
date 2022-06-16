cd70d8fba58fd3e5317bbbe2576d40bbd5be40f1
Levan Burchuladze <levan.burchuladze@rokolabs.com>
2022-06-16 22:35:59 +0400

    release text analyzer in console

---
+++ AnalyzerTextConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+
+var alphabetWords = String.Join(Environment.NewLine, GetAlphaWords(text));
+var palindromeWords = String.Join(Environment.NewLine, GetPalindrome(text));
+
+Console.WriteLine(Environment.NewLine + "Слова по алфавиту:");
+Console.WriteLine(alphabetWords);
+Console.WriteLine(Environment.NewLine + "Слова палиндромы:");
+Console.WriteLine(palindromeWords);
+
+List<String> GetAlphaWords(string text)
+{
+    List<String> words = new List<String>(GetOnlyTextWithoutSymbols(text).Split());
+    words = words.Distinct().ToList();
+    words.Sort();
+    return words;
+}
+
+String GetOnlyTextWithoutSymbols(string text)
+{
+    var newText = Regex.Replace(text, "[-.?!)(,:\n\r]", "");
+
+    return newText;
+}
+
+List<String> GetPalindrome(string text)
+{
+    List<String> words = new List<String>(GetOnlyTextWithoutSymbols(text).Split());
+    List<String> palindrome = new List<String>();
+
+    bool flag;
+
+    for (int i = 0; i < words.Count; i++)
+    {
+        flag = true;
+        for (int j = 0; j < words[i].Length / 2; j++)
+        {
+            string word = words[i];
+            if (word[j] != word[word.Length - j - 1])
+            {
+                flag = false;
+            }
+        }
+        if (flag)
+        {
+            palindrome.Add(words[i]);
+        }
+    }
+
+    return palindrome;
+}
